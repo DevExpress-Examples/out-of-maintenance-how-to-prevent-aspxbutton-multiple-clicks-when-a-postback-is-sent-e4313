89dd062233f0d6b7044b1b6c6f39cc4c45c989b7
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebSite/Default.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+public partial class _Default : System.Web.UI.Page {
+    protected void Page_Load(object sender, EventArgs e) {
+        if(IsPostBack)
+            // Emulate a long time postback.
+            Thread.Sleep(500);
+        else
+            Session["Count"] = null;
+        lbl.Text = GetPostBackCount();
+    }
+
+    private string GetPostBackCount() {
+        int count = 0;
+        if(Session["Count"] != null) {
+            count = (int)Session["Count"] + 1;
+            Session["Count"] = count;
+        } else
+            Session["Count"] = 0;
+        return count.ToString();
+    }
+
+    protected void btn_Click(object sender, EventArgs e) {
+        // Some code.
+    }
+}
